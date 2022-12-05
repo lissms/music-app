51ad3e8f16f810771738fe7948cdc5802b797e65
lissms <felissms@gmail.com>
2022-12-05 18:41:32 +0100

    install apollo/client

--- src/components/CardSong/types.ts
+++ src/components/CardSong/types.ts
@@ -1,19 +1,12 @@
-// import { Colors } from '$/styles/themes/theme';
-// import { typography } from '$/styles/themes/typography';
-// import { ReactNode } from 'react';
-
-// export type TVariants = keyof typeof typography;
+export interface ImageProps {
+  image: string;
+}
 
 export type CardSongProps = {
   image: string;
-  name?: string;
-  description?: string;
-  genre?: string;
-  author?: string;
-  isFavorite?: boolean;
+  name: string;
+  description: string;
+  genre: string;
+  author: string;
+  isFavorite: boolean;
 };
-
-// export type $StyledProps = {
-//   $variant: CardSongProps['variant'];
-//   $color: CardSongProps['color'];
-// };
