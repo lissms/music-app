44ac7233582b8a7ea145f17941c9cfeb88d0c121
lissms <felissms@gmail.com>
2022-12-06 07:33:24 +0100

    add ApolloProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,23 @@ import '$/styles/fonts.css';
 import { Layout } from '$/containers/Layouts';
 import GlobalStyle from '$/styles/global';
 import theme from '$/styles/themes';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  const client = new ApolloClient({
+    uri: 'https://api-frontend-challenge.herokuapp.com/graphql',
+    cache: new InMemoryCache(),
+  });
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
