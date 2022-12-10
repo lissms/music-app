79c2f988ced73bd4e14a92bee97547130fc3b202
lissms <felissms@gmail.com>
2022-12-10 22:39:46 +0100

    style to ProgressBar

--- src/components/AudioPlayer/styles.ts
+++ src/components/AudioPlayer/styles.ts
@@ -36,18 +36,51 @@ export const ContainerProgressBar = styled.div`
 `;
 
 export const ProgressBar = styled.input`
-  /* --bar-bg: #fabada;
-  --seek-before-width: 100px;
-  --seek-before-color: red;
-  --knobby: green;
-  --selected-Knobby: #000; */
-  appearance: none;
+  -webkit-appearance: none;
+  margin-right: 15px;
+  width: 200px;
+  height: 7px;
+  background: ${({ theme }) => theme.color.grayscale700};
+  border-radius: 5px;
+  background-image: linear-gradient(
+    ${({ theme }) => theme.color.malibu500},
+    ${({ theme }) => theme.color.malibu500}
+  );
+  /* background-size: 30% 100%; */
+  background-size: ${({ value, max }) =>
+      `${(Number(value) * 100) / Number(max)}%`},
+    100%;
+  background-repeat: no-repeat;
+  &::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    height: 10px;
+    width: 10px;
+    border-radius: 50%;
+    background: ${({ theme }) => theme.color.white};
+    cursor: ew-resize;
+
+    transition: background 0.2s ease-in-out;
+  }
+  &::-webkit-slider-runnable-track {
+    -webkit-appearance: none;
+    box-shadow: none;
+    border: none;
+    background: transparent;
+  }
+  /* appearance: none;
   background-color: ${({ theme }) => theme.color.grayscale700};
   border-radius: 1.25rem;
   position: relative;
   width: 7.75rem;
   height: 0.25rem;
-  outline: none;
+  outline: none; */
+
+  /* --bar-bg: #fabada;
+  --seek-before-width: 100px;
+  --seek-before-color: red;
+  --knobby: green;
+  --selected-Knobby: #000; */
+
   /* & :before {
     content: '';
     height: 4px;
@@ -102,7 +135,7 @@ export const ContainerImage = styled.div<Playing>`
   height: 3rem;
   border-radius: 0.75rem;
 `;
-export const ContainerButton = styled.div<Playing>`
+export const ContainerButton = styled.div`
   width: 8.5rem;
   display: flex;
   justify-content: space-between;
